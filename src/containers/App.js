5ca6c03981fa432dfb0c2fa74041c4362a8c7219
David (Xu Sheng) Gao <davidxsgao@gmail.com>
2021-03-27 19:46:19 -0400

    completed navbar with css linking img to home and welcome link to profile (#3)

--- src/containers/App.js
+++ src/containers/App.js
@@ -3,13 +3,16 @@ import { HashRouter as Router } from "react-router-dom";
 import Navbar from "../components/Navbar";
 import RouterSwitch from "../components/RouterSwitch";
 
-import { DATA } from "../data/data";
+import { DATA } from "../static/data/data";
 
 export default function App() {
+  if (!DATA) return <h1>Earthquake data could not be loaded</h1>;
+  const siteData = DATA.site;
+  const profileData = DATA.profile;
   return (
     <Router>
       <div className="body">
-        <Navbar data={DATA} />
+        <Navbar data={{ siteData, profileData }} />
         <RouterSwitch data={DATA} />
       </div>
     </Router>
